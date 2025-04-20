6eadb36b0e0f8a09536ca57174ec42bd9adc90ee
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-20 14:32:23 +0530

    Update StateSubsidy.java

--- StateSubsidy.java
+++ StateSubsidy.java
@@ -1,1 +1,8 @@
-
+// b11
+if (!hasData) {
+                System.out.println("No subsidy information found for the entered state.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+// b11
