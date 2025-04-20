60650c64869c5acfea9d6fdc7ebf0ce135598ce5
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-20 14:31:15 +0530

    Update StateSubsidy.java

--- StateSubsidy.java
+++ StateSubsidy.java
@@ -1,1 +1,14 @@
+// b10
+try {
+            ResultSet rs = DatabaseHelper.getStateSubsidies();
+            boolean hasData = false;
 
+            while (rs.next()) {
+                if (rs.getString("state_name").equalsIgnoreCase(stateName)) {
+                    hasData = true;
+                    System.out.println("State: " + rs.getString("state_name"));
+                    System.out.println("Subsidy Percentage: " + rs.getFloat("subsidy_percent") + "%");
+                    System.out.println("--------------------------------------");
+                }
+            }
+// b10
