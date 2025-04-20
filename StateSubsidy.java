d74730982e3d36a40aca1e0fa446008921e09924
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-20 14:32:58 +0530

    Update StateSubsidy.java

--- StateSubsidy.java
+++ StateSubsidy.java
@@ -1,1 +1,7 @@
-
+// b12
+// Add this line to prompt the user to press Enter before returning to the main menu
+        System.out.println("Press Enter to return to the main menu...");
+        scanner.nextLine();  // Wait for user to press Enter
+    }
+}
+// b12
