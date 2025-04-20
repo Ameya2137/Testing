9011d42b818f0843fd59c2f2a08a7f25d4e33b7f
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-20 14:30:22 +0530

    Update StateSubsidy.java

--- StateSubsidy.java
+++ StateSubsidy.java
@@ -1,1 +1,9 @@
+// b9
+import java.sql.*;
+import java.util.Scanner;
 
+public class StateSubsidy {
+    public static void run(Scanner scanner) { // Accept the scanner as an argument
+        System.out.print("Enter state name: ");
+        String stateName = scanner.nextLine();  // Use the passed scanner
+// b9
