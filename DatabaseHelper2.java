4723f47156047ab10ee9e6e779ff46885de5d183
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-20 14:11:28 +0530

    Update DatabaseHelper2.java

--- DatabaseHelper2.java
+++ DatabaseHelper2.java
@@ -1,1 +1,28 @@
+public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static ResultSet getProjects() throws SQLException {
+        return executeQuery("SELECT * FROM Projects");
+    }
+
+    public static ResultSet getPowerPurchaseAgreements() throws SQLException {
+        return executeQuery("SELECT * FROM PowerPurchaseAgreements");
+    }
+
+    public static ResultSet getSolarPanelData() throws SQLException {
+        return executeQuery("SELECT * FROM SolarPanelData");
+    }
+
+    public static ResultSet getStateSubsidies() throws SQLException {
+        return executeQuery("SELECT * FROM StateSubsidies");
+    }
+
+    public static ResultSet getSolarIntensity() throws SQLException {
+       return executeQuery("SELECT * FROM SolarIntensity");
+    }
+
+    public static ResultSet getSolarPanelModels() throws SQLException {
+       return executeQuery("SELECT panel_id, brand, model_name, power_output, cost_per_panel, length, width FROM SolarPanelModels");
+    }
 
